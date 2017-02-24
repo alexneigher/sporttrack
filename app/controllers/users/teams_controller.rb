3de106fb24ec08eff31c3578127a62e6d5ea82ee
Alex Neigher <alex.neigher@snapdocs.com>
2017-02-23 23:40:02 -0800

    nicer user show

--- app/controllers/users/teams_controller.rb
+++ app/controllers/users/teams_controller.rb
@@ -2,7 +2,7 @@ class Users::TeamsController < ApplicationController
   
   def index
     @user = User.includes(:teams).find(params[:user_id])
-    @teams = @user.teams
+    @teams = @user.teams.includes(:users)
   end
 
 end
